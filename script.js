713a216b78053812b937a0a1fa1d15648778fed2
Albert <albertzhou2005@gmail.com>
2018-01-13 17:45:31 -0800

    hi

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function loginin() { 
-   
+var person = {
+   firstName:"John",
+    lastName:"Face",
+    
+};
+var loginDetails = {
+    username:"1234",
+    password:"1234",
 }
- 
-loginin();
+function myFunction() { 
+    console.log(person.firstName)
+    console.log(person.lastName)
+    console.log(loginDetails.username)
+    console.log(loginDetails.password)
+    
+}
+myFunction();
