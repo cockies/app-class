3222f037e18fe0abbe4fc8597be4ff3365eae30b
Albert <albertzhou2005@gmail.com>
2018-02-17 17:11:34 -0800

    op

--- script.js
+++ script.js
@@ -1,176 +1,81 @@
-var x3=0;
-var y3=0;
-var x2=0;
-var y2=0;
-var offsetx2=-500;
-var offsety2=-200;
-var x=100;
-var y=100;
-var dx=1;
-var dy=1;
-var dx2=1;
-var dy2=1;
-var c = document.getElementById("myCanvas");
-var something = Math.PI*2;
-var ctx = c.getContext("2d");
-var colooor = "red";
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 50;
+var paddleWidth = 15;
+var paddleY = (canvas.height-paddleHeight)/2;
+var topPressed = false;
+var botPressed = false;
 
-function getMousePos(canvas, evt) {
-     
-    var rect = c.getBoundingClientRect();
-    return {
-        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
-    };
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 40) {
+        botPressed = true;
+    }
+    else if(e.keyCode == 38) {
+        topPressed = true;
+    }
 }
-
-
-function CollisionCheck(){
-    
-    var cx = (x2 + something) - (x3 + something);
-    
-    var cy = (y2 + something) - (y3 + something);
-              
-    var distance = Math.sqrt(cx * cx + cy * cy);
-    
-    if (distance < something + something) {
-        // collision detected!
-        colooor =  "green";
-         dx2*=-1;
-         dy2*=-1;
-    } else {
-        // no collision
-        colooor = "red"; 
+function keyUpHandler(e) {
+    if(e.keyCode == 40) {
+        botPressed = false;
+    }
+    else if(e.keyCode == 38) {
+        topPressed = false;
     }
 }
 
-
-function draw3()
-{
-    x3+=dx2;
-    y3+=dy2;
- 
-    
-    if (x3 > c.width || x3 < 0 ){  
-         dx2*=-1;
-         
-    
-    //console.log(dx)
-        //console.log("out of bounds (horizontal)")
-    }
-    
-    if (y3 > c.height || y3 < 0){
-        dy2*=-1;
-       //  console.log(dy)
-       // console.log("out of bbounds (vertical)")
-    }
-    
- 
-  ctx.beginPath();
-  ctx.fillStyle="#71f442";
-  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  ctx.arc(x3,y3,10,0,something,true);
-  ctx.closePath();
-  ctx.fill();
-
- // x+=dx;
-  //y+=dy;
-    
-  
-}
-
- c.addEventListener('mousemove', function(evt) {
-        var mousePos = getMousePos(c, evt);
-        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        //console.log(message);
-        
-        x2 = mousePos.x; 
-        y2 = mousePos.y;
-     
-      }, false);
-
-function handler(e) {
-    e = e || window.event;
-
-    var pageX = e.screenX;
-    var pageY = e.screenY;
-
-    // IE 8
-    if (pageX === undefined) {
-        pageX = e.clientX;
-        pageY = e.clientY;
-    }
-
-    //x2 = pageX + offsetx2; 
-    //y2 = pageY + offsety2;
-
-}
-
-
-
-document.addEventListener("mousemove", handler);
-
-ctx.beginPath();
-    ctx.fillStyle="#0000ff";
-    // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-    ctx.arc(100,100,20,0,something,true); ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(0, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-
-
-
-
-
-  
-
-
-
-function draw()
-{
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
     
-  ctx.beginPath();
-  ctx.fillStyle="#0000ff";
-  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  ctx.arc(x,y,20,0,something,true);
-  ctx.closePath();
-  ctx.fill();
-  x+=dx;
-  y+=dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    console.log(paddleY)
     
-  
+    if(topPressed && paddleY > 0) {
+           paddleY -= 6;
+      
+        
+        console.log("ok")
+    }
+      if( botPressed && paddleY < canvas.height) {
+       paddleY += 6;
+         
+        
+         console.log("999")
+    }
+    
+   
+    
+    x += dx;
+    y += dy;
 }
 
-
-function drawAll()
-{
-    context.clearRect(0,0, 300,300);
-    drawplayer();
-    draw();
-    draw3();
-     CollisionCheck();
-}
-
-function drawplayer()
-{
-  ctx.beginPath();
-  ctx.fillStyle= colooor;
-  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  ctx.arc(x2,y2,10,0,something,true);
-  ctx.closePath();
-  ctx.fill();
-//x2+=dx;
-  //y2+=dy;
-  
-}
-
-
-//1
-
-
-
-  context= myCanvas.getContext('2d');
-  setInterval(drawAll,10);
-
-
-
-
+setInterval(draw, 10);
