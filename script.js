41906be4da7039f49d225f6c89062bd65e2fc7a5
Albert <albertzhou2005@gmail.com>
2018-03-03 16:59:49 -0800

    ape

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
+var dx = 1;
+var GameOver = false;
 var dy = -2;
+var dAIy = 2;
 var paddleHeight = 50;
 var paddleWidth = 15;
 var paddleY = (canvas.height-paddleHeight)/2;
 var topPressed = false;
 var botPressed = false;
+var AIY = (canvas.height-paddleHeight)/2;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -41,15 +44,32 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(0, paddleY, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#ff0000";
     ctx.fill();
     ctx.closePath();
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
+
+
+function drawAIPaddle() {
+    ctx.beginPath();
+    ctx.rect(canvas.width - paddleWidth, AIY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0000ff";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawAI() {
+    //AIY = y - 20;
+    
+    
+    
+    if(AIY <= y-20){
+       AIY += dAIy;
+       }
+    else {
+       AIY -= dAIy;
+       }
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -57,19 +77,60 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    console.log(paddleY)
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(!GameOver){
+    drawBall();
+    drawAI();
+        
+       } else {
+           x = canvas.width/2;
+           y = canvas.height-30;
+           //GameOver = false;
+       }
+     drawPaddle();
+      drawAIPaddle();
+    
+    
+    /*
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }*/
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+        
+       // console.log("no")
+    }
+     
+   
+    if(paddleY + paddleHeight > y && paddleY < y && x < paddleWidth) {
+        
+        dx = -dx;
+    }
+    else{
+        //bad
+    }
+   
+    
+   
+    
+  
+    
+    
     
     if(topPressed && paddleY > 0) {
            paddleY -= 6;
       
         
-        console.log("ok")
+        
     }
-      if( botPressed && paddleY < canvas.height) {
+      if( botPressed && paddleY < canvas.height - paddleHeight) {
        paddleY += 6;
          
         
-         console.log("999")
+         
     }
     
    
